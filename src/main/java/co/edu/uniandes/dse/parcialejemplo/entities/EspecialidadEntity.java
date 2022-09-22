1005b324a653f66ce0860fc973448d8d6f5ea21e
Diego-Alejandro-Gonzalez-Vargas <da.gonzalezv1@uniandes.edu.co>
2022-09-22 12:00:08 -0500

    entidades

---
+++ src/main/java/co/edu/uniandes/dse/parcialejemplo/entities/EspecialidadEntity.java
@@ -0,0 +1,38 @@
+package co.edu.uniandes.dse.parcialejemplo.entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.Setter;
+import uk.co.jemos.podam.common.PodamExclude;
+import uk.co.jemos.podam.common.PodamStrategyValue;
+
+/**
+ * Clase que representa un autor en la persistencia
+ *
+ * @author da.gonzalezv1 - 202110240
+ */
+
+@Getter
+@Setter
+@Entity
+
+public class EspecialidadEntity {
+
+    private String nombre;
+    private String descripcion;
+
+    @PodamExclude
+	@ManyToMany(mappedBy = "especialidades")
+	private List<MedicoEntity> medicos = new ArrayList<>();
+    
+}
---
+++ src/main/java/co/edu/uniandes/dse/parcialejemplo/entities/MedicoEntity.java
@@ -0,0 +1,40 @@
+package co.edu.uniandes.dse.parcialejemplo.entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.Setter;
+import uk.co.jemos.podam.common.PodamExclude;
+import uk.co.jemos.podam.common.PodamStrategyValue;
+
+/**
+ * Clase que representa un autor en la persistencia
+ *
+ * @author da.gonzalezv1 - 202110240
+ */
+
+@Getter
+@Setter
+@Entity
+
+public class MedicoEntity {
+    private String nombre;
+    private String apellido;
+    private Integer registromedico;
+
+    @PodamExclude
+	@ManyToMany
+	private List<EspecialidadEntity> especialidades = new ArrayList<>();
+
+
+    
+}
